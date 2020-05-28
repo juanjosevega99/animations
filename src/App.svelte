66f6d64303ae3506b5b7be8f19b1038957c4f2e0
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-05-27 20:43:31 -0500

    class animation

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,64 @@
 <script>
-	export let name;
+	
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+	.characters {
+		width: 100%;
+		display: grid;
+		grid-template-columns: repeat(5, 1fr);
+		grid-gap: 8px
+	}
+	figure img {
+		width: 100%;
+		margin: 0;
+	}
+	/* .loading {
+		background-color: aqua;
+		width: 100px;
+		height: 100px;
+		animation-name: coloranimate;
+		animation-duration: 4s;
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+	@keyframes coloranimate {
+		from {
+			background-color: red;
+		}
+		to {
+			background-color: black;
+		}
+	} */
+
+	.loading {
+		width: 100px;
+		height: 100px;
+		background-color: black;
+		border-radius: 50%;
+		border: 20px solid red;
+		border-top: 20px solid white;
+		animation: loader 2s liner infinite;
 	}
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
+	@keyframes loader {
+		0% {
+			transform: rotate(0deg);
+		}
+		100% {
+			transform: rotate(360deg);
 		}
 	}
 </style>
+
+<div class="characters">
+	{#each characters as character}
+		<figure>
+
+		</figure>
+	{/each}
+	<div class="loading"></div>
+</div>
